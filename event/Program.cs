aee0ccffdedd99ede7535bb3feb212bfdba8e0d7
hephzi06 <hephzisoft@gmail.com>
2022-07-20 14:25:43 +0100

    made some changes

---
+++ event/Program.cs
@@ -0,0 +1,23 @@
+namespace Work
+
+{
+    // creating a delegates
+    public delegate double CalcVolume(double a);
+    public delegate double CalcArea(double b);
+    // our class
+
+    class Program
+    {
+        //  our methods for addition
+
+
+        public static void Main(string[] args)
+        {
+            // variables
+
+
+        }
+
+
+    }
+}
