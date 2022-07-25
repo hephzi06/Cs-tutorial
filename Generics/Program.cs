58b8902db24edd759553292bb32c0517134aade9
hephzi06 <hephzisoft@gmail.com>
2022-07-25 17:15:36 +0100

    made some changes

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+class ReadOnly
+{
+    public static void Main(string[] args)
+    {
+        List<string> objlist = new List<string>();
+        objlist.Add("Duyile");
+        objlist.Add("Thompson");
+        objlist.Add("Hephzibah");
+        objlist.Add("Lanre");
+        objlist.Add("Emma");
+        objlist.Add("Chrsiti");
+        ReadOnlyCollection<string> obj2 = new ReadOnlyCollection<string>(objlist);
+        foreach (string item in obj2)
+        {
+            Console.WriteLine($"{item}");
+        }Console.WriteLine("The total number of items in the list is : {obj2.Count} ");
+        if (objlist.Contains("hephzi"))
+        {
+            objlist.Insert(2, "Jude");
+        }
+    }
+}
--- class4/Program.cs
+++ class4/Program.cs
@@ -1,2 +1,532 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 1
+namespace AbeoukutaNorth
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "Lowland";
+       protected int population = 20000;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+
+   }
+}
+// 2
+
+namespace AbeoukutaSouth
+{
+   class Geography
+   {
+       protected int landmass = 200000;
+       protected string topography = "Midland";
+       protected int population = 10000;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+   }
+}
+// 3
+namespace Ado_Odo_Ota
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "beach";
+       protected int population = 20;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+
+   }
+}
+
+// 4
+namespace Ewekoro
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "beach";
+       protected int population = 20;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+   }
+}
+// 5
+namespace Ifo
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "beach";
+       protected int population = 20;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+   }
+}
+// 6
+namespace IjebuEast
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "beach";
+       protected int population = 20;
+       protected string weather = "rainy";
+       protected string climate = "tropical";
+
+   }
+   class Economy : Geography
+   {
+       string S_O_E = "Limestone";
+       public void display()
+       {
+           Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+       }
+
+   }
+}
+// 7
+namespace IjebuNorth
+{
+   class Geography
+   {
+       protected int landmass = 20000;
+       protected string topography = "beach";
+       protected int population = 20;
+       protected string weather = "rainy";
+       protected string climate = "tropical";}
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+    }
+}
+// 8
+namespace IjebuNorthEast
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 9
+namespace IjebuOde
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+    }
+}
+// 10
+namespace Ikenne
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+    }
+}
+// 11
+namespace Imeko
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 12
+
+namespace Ipokia
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+    }
+}
+// 13
+namespace Owode
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}// 14
+namespace Odeda
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 15
+namespace Odogbolu
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 16
+namespace OgunWaterSide
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 17
+namespace RemoNorth
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 18
+namespace Sagamu
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 19
+namespace YewaNorth
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";
+
+    }
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+// 20
+namespace YewaSouth
+{
+    class Geography
+    {
+        protected int landmass = 20000;
+        protected string topography = "beach";
+        protected int population = 20;
+        protected string weather = "rainy";
+        protected string climate = "tropical";}
+    class Economy : Geography
+    {
+        string S_O_E = "Limestone";
+        public void display()
+        {
+            Console.Write($"Landmass = {landmass} \n  Topography = {topography} \n Population = {population} \n  Weather = {weather} \n Climate = {climate} \n Source of Income  = {S_O_E}");
+        }
+
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Welcome to Ogun State Survey\n Select a Local Government\n1. AbeoukutaNorth\n2. AbeoukutaSouth\n3. Ado_Odo_Ota\n4. Ewekoro\n5. Ifo\n6. IjebuEast\n7.IjebuNorth\n8. IjebuNorthEast\n9. IjebuOde \n10. Ikenne \n11. Imeko \n12. Ipokia\n  13. Owode\n14. Odeda\n15. Odogbolu\n16. OgunWaterSide\n17. RemoNorth\n18. Sagamu\n19. YewaNorth\n20.YewaSouth ");
+        int option = Convert.ToInt32(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                AbeoukutaNorth.Economy dis1 = new AbeoukutaNorth.Economy();
+                dis1.display();
+                break;
+            case 2:
+                AbeoukutaSouth.Economy dis2 = new AbeoukutaSouth.Economy();
+                dis2.display();
+                break;
+            case 3:
+                Ado_Odo_Ota.Economy dis3 = new Ado_Odo_Ota.Economy();
+                dis3.display();
+                break;
+            case 4:
+                Ewekoro.Economy dis4 = new Ewekoro.Economy();
+                dis4.display();
+                break;
+            case 5:
+                Ifo.Economy dis5 = new Ifo.Economy();
+                dis5.display();
+                break;
+            case 6:
+                Ewekoro.Economy dis6 = new Ewekoro.Economy();
+                dis6.display();
+                break;
+            case 7:
+                IjebuEast.Economy dis7 = new IjebuEast.Economy();
+                dis7.display();
+                break;
+            case 8:
+                IjebuNorthEast.Economy dis8 = new IjebuNorthEast.Economy();
+                dis8.display();
+                break;
+            case 9:
+                IjebuOde.Economy dis9 = new IjebuOde.Economy();
+                dis9.display();
+                break;
+            case 10:
+                Ikenne.Economy dis10 = new Ikenne.Economy();
+                dis10.display();
+                break;
+            case 11:
+                Imeko.Economy dis11 = new Imeko.Economy();
+                dis11.display();
+                break;
+            case 12:
+                Ipokia.Economy dis12 = new Ipokia.Economy();
+                dis12.display();
+                break;
+            case 13:
+                Owode.Economy dis13 = new Owode.Economy();
+                dis13.display();
+                break;
+            case 14:
+                Odeda.Economy dis14 = new Odeda.Economy();
+                dis14.display();
+                break;
+            case 15:
+                Odogbolu.Economy dis15 = new Odogbolu.Economy();
+                dis15.display();
+                break;
+            case 16:
+                OgunWaterSide.Economy dis16 = new OgunWaterSide.Economy();
+                dis16.display();
+                break;
+            case 17:
+                RemoNorth.Economy dis17 = new RemoNorth.Economy();
+                dis17.display();
+                break;
+            case 18:
+                Sagamu.Economy dis18 = new Sagamu.Economy();
+                dis18.display();
+                break;
+            case 19:
+                YewaNorth.Economy dis19 = new YewaNorth.Economy();
+                dis19.display();
+                break;
+            case 20:
+                YewaSouth.Economy dis20 = new YewaSouth.Economy();
+                dis20.display();
+                break;
+
+                // default:
+                // default:
+                // default:
+                //   Console.Write("Invalid choice");
+        }
+    }
+}
