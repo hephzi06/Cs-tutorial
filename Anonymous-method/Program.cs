b7541b06e0bdf63067c5400de1593860038dc659
hephzi06 <hephzisoft@gmail.com>
2022-07-26 19:23:34 +0100

    made some changes

---
+++ Anonymous-method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+public delegate void NumChecker();
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var textInfo = CultureInfo.CurrentCulture.TextInfo;
+        var state = new[] {
+        new {statename = "ogun state", state_capital = "abeoukta",governor = "abiodun"},
+        new {statename = "lagos state", state_capital = "ikeja",governor = "sanwo-Olu"},
+        new {statename = "oyo state", state_capital = "ibadan",governor = "makinde"}
+        };
+        Console.WriteLine("Welcome to state wiki");
+        Console.Write("Enter a state name: ");
+        string option = Console.ReadLine().ToLower();
+        for (int i = 0; i <= state.Length; i++)
+        {
+            if (state[i].statename == option)
+            {
+                Console.WriteLine($" {textInfo.ToTitleCase(state[i].statename)} {state[i].state_capital} {state[i].governor}");
+                break;
+            }
+        }
+
+
+    }
+}
