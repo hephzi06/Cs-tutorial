d74d552e009f8d444fc19dbb98eb5e4f6dca3a9a
hephzi06 <hephzisoft@gmail.com>
2022-07-28 10:36:30 +0100

    added new folder

---
+++ new/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Name
+{
+    class Program{
+        public static void Main(string[] args){
+            string demo = "Hello World Eche";
+            demo.Say();
+        }
+    }
+
+    public static class ExtensionMethod{
+        public static void Say(this string message){
+            Console.WriteLine(message);
+        }
+    }
+}
